cb3fcac2cf775c5ad1b632237ef3c28ae3953ec3
Jason Stumpf <jstumpf518@yahoo.com>
2023-05-25 10:17:06 -0700

    First Commit

---
+++ new_module.py
@@ -0,0 +1,10 @@
+def square_root(length, width):
+    area = length * width
+    return area
+
+from math import pi as p
+
+def circle(radius):
+    circumference = (2 * p) * radius
+    return circumference
+
